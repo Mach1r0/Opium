3cfa072c0d8119bc62ea1f8ff693b0f9325aada7
Beatriz-ge <bibi.geane2@gmail.com>
2024-08-07 11:18:24 -0300

    feat: contact us page

--- frontend/src/app/contactus/page.tsx
+++ frontend/src/app/contactus/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from '../style/contactus.module.css'
 import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 
 export default function Contactus() {
@@ -11,14 +12,17 @@ export default function Contactus() {
                 <h2 className={style['title']}>FALE CONOSCO</h2>
                 <p className={style['text']}>Entre em contato por meio de nossas redes sociais. O atendimento está disponível das 8:00 às 18:30 horas de segunda à sábado.</p>
             </div>
-            <div>
-                <button className={style['btn-whatsapp']}>
-                    <i className={style['contactus-icon']}></i>
-                    <FaWhatsapp />
 
-                    Atendimento via Whatsapp
-                </button>
-            </div>
+            <button className={style['btn-aboutus']}>
+                <FaWhatsapp className={style['contactus-icon']} />
+                Atendimento via Whatsapp
+            </button>
+
+            <button className={style['btn-aboutus']}>
+                <FaInstagram className={style['contactus-icon']} />
+                Atendimento via Instagram
+            </button>
+            
         </section>
 
     </main>
