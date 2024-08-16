e02ad07a7f05d866ceb869d63af39e842e658a63
Beatriz-ge <bibi.geane2@gmail.com>
2024-08-16 11:09:56 -0300

    feat: navbar and footer visibility on sign in and sign up pages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import './globals.css'; 
+// src/app/layout.tsx
+'use client';
+
+import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
+import { usePathname } from 'next/navigation';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <html lang="en">
-            <body>
-                <Navbar />
-                <main>{children}</main>
-                <Footer />
-            </body>
-        </html>
-    );
-};
+  const pathname = usePathname();
+
+  const hideNavbarFooter = pathname === '/sign-in' || pathname === '/sign-up';
+
+  return (
+    <html lang="en">
+      <body>
+        {!hideNavbarFooter && <Navbar />}
+        {children}
+        {!hideNavbarFooter && <Footer />}
+      </body>
+    </html>
+  );
+}
 
 export default Layout;
